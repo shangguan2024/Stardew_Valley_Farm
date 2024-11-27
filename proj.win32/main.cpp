569c40dbd65f544ea41dfa6204cc29905fd2c678
Zhang Xiang <144351162+Muoow@users.noreply.github.com>
2024-11-27 23:38:17 +0800

    Modify

--- proj.win32/main.cpp
+++ proj.win32/main.cpp
@@ -28,10 +28,7 @@
 
 USING_NS_CC;
 
-int WINAPI _tWinMain(HINSTANCE hInstance,
-                       HINSTANCE hPrevInstance,
-                       LPTSTR    lpCmdLine,
-                       int       nCmdShow)
+int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
 {
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
